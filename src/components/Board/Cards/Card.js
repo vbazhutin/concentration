a942e1c1321eb94c6785cc63cd88b698b46c8340
Manav Misra <manavm@visionwebsoft.com>
2020-08-11 14:23:07 -0500

    ♿Resolve a11y warning regarding click event
    Including some code formatting

--- src/components/Board/Cards/Card.js
+++ src/components/Board/Cards/Card.js
@@ -1,18 +1,27 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-export const Card = ({ code, id, image, flipped, matched, suit, value, handler }) => {
+export const Card = ({
+  code,
+  id,
+  image,
+  flipped,
+  matched,
+  suit,
+  value,
+  handler,
+}) => {
   return (
-    <img
-      src={flipped ? image : "https://source.unsplash.com/random/226x314"}
-      alt={`${value} of ${suit}`}
-      onClick={handler}
-      // 'code' will be used to just compare the 'card values'
-      data-code={code}
-
-      // id will be used to id the correct card in our Array of cards (dataset.id)
-      data-id={id}
-    />
+    <button onClick={handler}>
+      <img
+        src={flipped ? image : "https://source.unsplash.com/random/226x314"}
+        alt={`${value} of ${suit}`}
+        // 'code' will be used to just compare the 'card values'
+        data-code={code}
+        // id will be used to id the correct card in our Array of cards (dataset.id)
+        data-id={id}
+      />
+    </button>
   )
 }
 
