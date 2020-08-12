78b4b980013e8c54103c5aa27228db71d0a0b6e7
Manav Misra <manavm@visionwebsoft.com>
2020-08-12 14:33:08 -0500

    Flip the cards when clicked and remove matches
    Add flipped cards if there are not duplicates and we don't have 2
    
    
    :fire: Remove previous code that was adding more useEffects
    
    
    Log match if the flipped cards codes are equal
    
    
    Add a matched code whenever there are 2 matching flipped cards
    
    
    Flip, match cards using CSS animation

--- src/components/Board/Cards/index.js
+++ src/components/Board/Cards/index.js
@@ -3,40 +3,35 @@ import React, { useState, useEffect } from "react"
 
 import { Card } from "./Card"
 
-
 import "./Cards.css"
 
 export const Cards = ({ cards, handler }) => {
   // This will just manage flipped and matched cards
   const [flippedCards, setFlippedCards] = useState([])
+  const [matchedCards, setMatchedCards] = useState([])
 
-  // useEffect(() => {
-  //   setCards((prevCards) =>
-  //     prevCards.map((card) => {
-  //       if (card.id === flippedCards[0].id || card.id === flippedCards[1]?.id) {
-  //         card.flipped = true
-  //       }
-  //       return card
-  //     })
-  //   )
+  useEffect(() => {
+    if (flippedCards.length && flippedCards[0].code === flippedCards[1]?.code) {
+      setMatchedCards((prevMatched) =>
+        prevMatched.concat(flippedCards[0]?.code)
+      )
+    }
+  }, [flippedCards])
 
-  //   if (flippedCards[0]?.code === flippedCards[1]?.code) {
-  //     cards.map((card) => {
-  //       if (card.id === flippedCards[0].id || card.id === flippedCards[1]?.id) {
-  //         card.matched = true
-  //         return card
-  //       }
-  //     })
-  //   }
-  // }, [flippedCards])
-
+  // if matchedCards.length = cards.length / 2, then stop the timer
   const flipHandler = ({ target: { dataset } }) => {
-    // If it's true that there is no length on flippedCards...
+    // If it's true that there is no length of flippedCards...
     if (!flippedCards.length) {
       setFlippedCards((flippedCards) =>
-        flippedCards.concat({ id: dataset.id, code: dataset.code })
+        // 'id' is for uniquely id this card while code is for comparing matching values
+        flippedCards.concat({
+          id: dataset.id,
+          code: dataset.code,
+        })
       )
-    } else if (flippedCards[0].id !== dataset.id) {
+    }
+    // Only the card if it's not the same AND if we don't already have 2 cards
+    else if (flippedCards[0].id !== dataset.id && flippedCards.length < 2) {
       setFlippedCards((flippedCards) =>
         flippedCards.concat({
           id: dataset.id,
@@ -48,18 +43,30 @@ export const Cards = ({ cards, handler }) => {
 
   const renderCards = () =>
     // 'suit' and 'value' are just for alt tag
-    cards.map(({ code, matched, id, image, suit, value }, i) => (
-      <Card
-        code={code}
-        id={id}
-        image={image}
-        matched={matched}
-        suit={suit}
-        value={value}
-        handler={flipHandler}
-        key={i}
-      />
-    ))
+    cards.map(({ code, flipped, matched, id, image, suit, value }, i) => {
+      // If the current card is 'listed' in flippedCards, lets set it as flipped before it gets rendered
+      if (id === flippedCards[0]?.id || id === flippedCards[1]?.id) {
+        flipped = true
+      }
+
+      if (matchedCards.includes(code)) {
+        matched = true
+      }
+
+      return (
+        <Card
+          code={code}
+          flipped={flipped}
+          id={id}
+          image={image}
+          matched={matched}
+          suit={suit}
+          value={value}
+          handler={flipHandler}
+          key={i}
+        />
+      )
+    })
 
   return <div className="container">{renderCards()}</div>
 }
