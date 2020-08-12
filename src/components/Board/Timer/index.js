9a2717b21f9e2b34b3e1a9c359b46be82196c87f
Manav Misra <manavm@visionwebsoft.com>
2020-08-12 16:52:15 -0500

    Show timer in minutes and seconds

--- src/components/Board/Timer/index.js
+++ src/components/Board/Timer/index.js
@@ -1,10 +1,29 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types"
+import React, { useEffect, useState } from "react"
 
-export const Timer = ({ toggle }) => <p>Timer</p>
+export const Timer = ({ toggle }) => {
+  const [time, setTime] = useState(0)
+
+  const convertSecondsToMinutesAndSeconds = (secs) =>
+    new Date(secs * 1000).toISOString().substring(14, 19)
+
+  useEffect(() => {
+    while (toggle) {
+      const intervalID = setInterval(() => {
+        setTime(() => time + 1)
+      }, 1000)
+      // Cleanup function
+      return () => {
+        clearInterval(intervalID)
+      }
+    }
+  })
+
+  return <p>{convertSecondsToMinutesAndSeconds(time)}</p>
+}
 
 Timer.propTypes = { toggle: PropTypes.bool }
 
 Timer.defaultProps = {
-  toggle: false
+  toggle: false,
 }
