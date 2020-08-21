07b5f9829f4f532e0bbc34fbb95d02752ac36ff1
Manav Misra <manavm@visionwebsoft.com>
2020-08-21 11:15:47 -0500

    Use form component to set the number of pairs

---
+++ src/components/Concentration/Form.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+import React from "react"
+
+export const Form = ({handler}) => {
+  return (
+    <form
+      className="my-flex my-flex--align-center my-flex--justify-center"
+      onSubmit={handler}
+    >
+      <div>
+        <label htmlFor="pairsOfCards">How many pairs of cards?</label>
+        <input
+          type="number"
+          id="pairsOfCards"
+          placeholder="(Default: 4)"
+          className="mx-2"
+        />
+      </div>
+      <button className="button is-rounded">Go!</button>
+    </form>
+  )
+}
+
+Form.propTypes = {
+  handler: PropTypes.func
+}
--- src/components/Concentration/index.js
+++ src/components/Concentration/index.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react"
 
+import { Form } from "./Form"
 import { Cards } from "./Cards"
 import { Timer } from "./Timer"
 
 export const Concentration = () => {
+  const [pairsOfCards, setPairsOfCards] = useState("4")
   const [toggleTimer, setToggleTimer] = useState(false)
 
   const handleCards = (toggle) => {
     setToggleTimer(toggle)
   }
 
+  const handleForm = (event) => {
+    event.preventDefault()
+    setPairsOfCards(event.target.elements[0].value)
+  }
+
   return (
     <main>
-      <Cards handler={handleCards} />
+      <Form handler={handleForm} />
+      <Cards handler={handleCards} pairs={pairsOfCards} />
       <Timer toggle={toggleTimer} />
     </main>
   )
