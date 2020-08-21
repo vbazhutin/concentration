d212166460d54fcb5053d30d1bd42bf647afecfd
Manav Misra <manavm@visionwebsoft.com>
2020-08-21 11:29:08 -0500

    Set 'h1' to nagivate back home

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,13 +3,15 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 export const Welcome = () => (
-  <header className="has-background-primary has-text-centered has-text-light py-4">
-    <h1>
-      Concentration!&nbsp;
-      <span role="img" aria-label="brain">
-        🧠
-      </span>
-    </h1>
+  <header className="has-background-primary has-text-centered py-4">
+    <Link to="/" className="has-text-light">
+      <h1>
+        Concentration!&nbsp;
+        <span role="img" aria-label="brain">
+          🧠
+        </span>
+      </h1>
+    </Link>
     <Link to="/game" className="mr-2">
       Start Game!
     </Link>
