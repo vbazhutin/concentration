5a0b431fc6e0261de66f95f126cf3856ad8e8cad
Manav Misra <manavm@visionwebsoft.com>
2020-08-21 10:34:53 -0500

    :art: Format code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './App';
+
 import "bulma/css/bulma.css"
 import './index.css';
-import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
